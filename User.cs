64253e42c13c0063313d80e9469ac1049432e3c1
natalya_kosyakina <anckamarckowa@yandex.ru>
2024-03-17 15:41:03 +0300

    Библиотека моделей к чату

---
+++ User.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatDB
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Login { get; set; }
+        public virtual ICollection<Message> IncomingMessages { get; set; }
+        public virtual ICollection<Message> OutgoingMessages { get; set; }
+
+    }
+}
